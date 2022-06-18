dfd3670dda36601bb3c9fd896f6f443b0133961e
DSilva27 <david.silvas@udea.edu.co>
2022-06-17 21:39:48 -0400

    set code to work with config files

--- cluster/sbi_train_posterior_cpu.py
+++ cluster/sbi_train_posterior_cpu.py
@@ -5,26 +5,35 @@ from sbi import utils as utils
 from sbi.inference import SNPE
 from sbi.utils.get_nn_models import posterior_nn
 
+
 def main(argv):
 
     torch.set_num_threads(int(argv[1]))
 
-    images = torch.load("images_1e6_128.pt")
-    indices = torch.load("indices_1e6_128.pt")
+    images = torch.load("images_1e6_128_noise_01.pt")
+    indices = torch.load("indices_1e6_128_noise_01.pt")
 
-    prior_indices = utils.BoxUniform(low=1*torch.ones(1), high=20*torch.ones(1))
+    sufix_posterior = "_128_noise_01_100_8"
 
-    density_estimator_build_fun = posterior_nn(model='maf', hidden_features=10, num_transforms=4)
-    inference = SNPE(prior=prior_indices, density_estimator=density_estimator_build_fun)
+    prior_indices = utils.BoxUniform(
+        low=1 * torch.ones(1), high=20 * torch.ones(1)
+    )
+
+    density_estimator_build_fun = posterior_nn(
+        model="maf", hidden_features=100, num_transforms=8
+    )
+    inference = SNPE(
+        prior=prior_indices, density_estimator=density_estimator_build_fun
+    )
 
     # Train multiple posteriors for different noise levels
-   
+
     inference = inference.append_simulations(indices, images)
 
     density_estimator = inference.train()
     posterior = inference.build_posterior(density_estimator)
 
-    with open("posterior_noise_rot_128.pkl", "wb") as handle:
+    with open("posterior" + sufix_posterior + ".pkl", "wb") as handle:
         pickle.dump(posterior, handle)
 
 
