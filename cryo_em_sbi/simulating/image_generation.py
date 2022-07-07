fix bug in quaternion generation

cryo_em_sbi/simulating/image_generation.py
@@ -11,10 +11,10 @@ def gen_quat():
         quat = np.random.uniform(
             -1, 1, 4
         )  # note this is a half-open interval, so 1 is not included but -1 is
-        norm = 1 / np.sqrt(np.sum(quat**2))
+        norm = np.sqrt(np.sum(quat**2))
 
         if 0.2 <= norm <= 1.0:
-            quat *= norm
+            quat /= norm
             count += 1
 
     return quat