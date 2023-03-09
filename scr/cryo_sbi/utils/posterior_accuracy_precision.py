cc4d8da755f24d91d04769f48a24e241f447d070
Dingel321 <dingel321@gmail.com>
2023-03-09 17:16:25 +0100

    added types of noise and outline of util function

---
+++ scr/cryo_sbi/utils/posterior_accuracy_precision.py
@@ -0,0 +1,13 @@
+from estimator_utils import sample_posterior
+from wpa_simulator.cryo_em_simulator import CryoEmSimulator
+
+
+def posterior_accuracy_precision(estimator, image_config, num_samples, num_posterior_samples, alpha):
+    # !!! needs image config which also generates different kinds of noise
+    # draw num_samples prior samples
+    # run simulations with prior samples
+    # draw num_posterior_samples from posterior
+    # compute mean posterior - true index
+    # compute confidence intervall
+    # retrurn mean posterior - true index, confidence intervall
+    pass
