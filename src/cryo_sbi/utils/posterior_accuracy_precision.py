b3fdd3068b94834911abe11cde780fe2333f4523
Dingel321 <dingel321@gmail.com>
2023-03-17 19:08:28 +0100

    this commit should resolve merge

--- src/cryo_sbi/utils/posterior_accuracy_precision.py
+++ src/cryo_sbi/utils/posterior_accuracy_precision.py
@@ -1,8 +1,10 @@
 from estimator_utils import sample_posterior
-from cryo_sbi import CryoEmSimulator
+from wpa_simulator.cryo_em_simulator import CryoEmSimulator
 
 
-def posterior_accuracy_precision(estimator, train_config, image_config, num_samples, num_posterior_samples):
+def posterior_accuracy_precision(
+    estimator, image_config, num_samples, num_posterior_samples, alpha
+):
     # !!! needs image config which also generates different kinds of noise
     # draw num_samples prior samples
     # run simulations with prior samples
