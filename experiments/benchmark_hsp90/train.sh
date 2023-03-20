b282d488b40ed682ec7528fc1612f89289dde194
Dingel321 <dingel321@gmail.com>
2023-03-20 17:50:29 +0100

    added indexing=ij to ctf calculation in order to remove warning

--- experiments/benchmark_hsp90/train.sh
+++ experiments/benchmark_hsp90/train.sh
@@ -1,6 +1,6 @@
 python3 -m cryo_sbi.inference.NPE_train_without_saving \
-    --image_config_file experiments/benchmark_hsp90/image_params_training.json \
-    --train_config_file experiments/benchmark_hsp90/resnet18_encoder.json\
+    --image_config_file benchmark_hsp90/image_params_training.json \
+    --train_config_file benchmark_hsp90/resnet18_encoder.json\
     --epochs 200 \
-    --estimator_file experiments/benchmark_hsp90/posterior_hsp90.estimator \
-    --loss_file experiments/benchmark_hsp90/posterior_hsp90.loss
+    --estimator_file benchmark_hsp90/posterior_hsp90.estimator \
+    --loss_file benchmark_hsp90/posterior_hsp90.loss
