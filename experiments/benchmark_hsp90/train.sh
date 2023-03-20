94cd6f3c27b968d4e74f6e30e53b40da5f817baf
Dingel321 <dingel321@gmail.com>
2023-03-20 17:31:09 +0100

    changed hsp90 experiment setup

--- experiments/benchmark_hsp90/train.sh
+++ experiments/benchmark_hsp90/train.sh
@@ -1,8 +1,6 @@
 python3 -m cryo_sbi.inference.NPE_train_without_saving \
     --image_config_file experiments/benchmark_hsp90/image_params_training.json \
     --train_config_file experiments/benchmark_hsp90/resnet18_encoder.json\
-    --epochs 150 \
-    --estimator_file experiments/benchmark_hsp90/posterior_hsp90_ex.estimator \
-    --loss_file experiments/benchmark_hsp90/posterior_hsp90_ex.loss \
-    --train_from_checkpoint \
-    --state_dict_file experiments/benchmark_hsp90/posterior_hsp90.estimator
+    --epochs 200 \
+    --estimator_file experiments/benchmark_hsp90/posterior_hsp90.estimator \
+    --loss_file experiments/benchmark_hsp90/posterior_hsp90.loss
