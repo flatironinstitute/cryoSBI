17ab0ae0610827cb48ee950b9cb703c591411bb4
Dingel321 <dingel321@gmail.com>
2023-03-21 18:05:26 +0100

    small changes to notebooks and config files

--- experiments/6wxb/train.sh
+++ experiments/6wxb/train.sh
@@ -1,8 +1,7 @@
-cd ../../scr/cryo_sbi/inference
-
-python NPE_train_without_saving.py \
-    --image_config_file ../../../experiments/6wxb/image_params_snr01_128.json \
-    --train_config_file ../../../experiments/6wxb/resnet18_encoder.json \
+python3 -m cryo_sbi.inference.NPE_train_without_saving \
+    --image_config_file 6wxb/image_params_training.json \
+    --train_config_file 6wxb/resnet18_encoder.json\
     --epochs 300 \
-    --estimator_file ../../../experiments/6wxb/6wxb_resnet18.estimator \
-    --loss_file ../../../experiments/6wxb/6wxb_resnet18.loss
+    --estimator_file 6wxb/posterior_6wxb.estimator \
+    --loss_file 6wxb/posterior_6wxb.loss \
+    --n_workers 24
