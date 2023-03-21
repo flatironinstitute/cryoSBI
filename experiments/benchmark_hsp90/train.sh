e539a146ba85ce8b001669cb53b903243a95743e
Dingel321 <dingel321@gmail.com>
2023-03-21 18:05:26 +0100

    small changes to notebooks and config files

--- experiments/benchmark_hsp90/train.sh
+++ experiments/benchmark_hsp90/train.sh
@@ -3,4 +3,5 @@ python3 -m cryo_sbi.inference.NPE_train_without_saving \
     --train_config_file benchmark_hsp90/resnet18_encoder.json\
     --epochs 200 \
     --estimator_file benchmark_hsp90/posterior_hsp90.estimator \
-    --loss_file benchmark_hsp90/posterior_hsp90.loss
+    --loss_file benchmark_hsp90/posterior_hsp90.loss \
+    --n_workers 24
