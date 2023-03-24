4f2d384c1a164b1d5cb1f4f1caf3eb2114eab403
Dingel321 <dingel321@gmail.com>
2023-03-24 18:46:07 +0100

    added noise different types of noise to noise.py

--- src/cryo_sbi/wpa_simulator/noise.py
+++ src/cryo_sbi/wpa_simulator/noise.py
@@ -5,7 +5,7 @@ import torch
 def circular_mask(n_pixels, radius):
     grid = torch.linspace(-0.5 * (n_pixels - 1), 0.5 * (n_pixels - 1), n_pixels)
     r_2d = grid[None, :] ** 2 + grid[:, None] ** 2
-    mask = r_2d < radius ** 2
+    mask = r_2d < radius**2
 
     return mask
 
@@ -36,13 +36,42 @@ def add_noise(image, image_params, seed=None):
     return image_noise
 
 
+def add_colored_noise(img, image_params, noise_scale):
+    """Adds colored noise to image"""
+    # Similar to pink noise https://en.wikipedia.org/wiki/Pink_noise
+
+    image_L = img.shape[0]
+
+    mask = circular_mask(n_pixels=img.shape[0], radius=image_params["RADIUS_MASK"])
+
+    signal_std = img[mask].pow(2).mean().sqrt()
+    noise_std = signal_std / np.sqrt(image_params["SNR"])
+
+    img_noise = torch.distributions.normal.Normal(0, noise_std).sample(img.shape)
+    fft_noise = torch.fft.fft2(img_noise)
+
+    along_x, along_y = np.linspace(-1, 1, image_L), np.linspace(-1, 1, image_L)
+    mesh_x, mesh_y = np.meshgrid(along_x, along_y)
+    f = torch.zeros((image_L, image_L))
+
+    for ix in range(image_L):
+        for iy in range(image_L):
+            f[ix, iy] = (
+                np.abs(mesh_x[ix, iy]) ** noise_scale
+                + np.abs(mesh_y[ix, iy]) ** noise_scale
+            )
+
+    t = torch.abs(torch.fft.ifft2(fft_noise / f))
+
+    noise_scale = 1.0 / (t.max() - t.median())
+    t = ((t - t.median()) * noise_scale) + 1
+
+    img_noise = torch.distributions.normal.Normal(0, noise_std * t).sample()
+    return img_noise + img
+
+
 def add_shot_noise(image):
     """Adds shot noise to image"""
-    pass
-
-
-def add_colored_noise(image):
-    """Adds colored noise to image"""
     pass
 
 
