1b49e403ec85c0da9682c757a300818337042c2a
Dingel321 <dingel321@gmail.com>
2023-03-31 15:31:29 +0200

    added images utils with function for torch transforms

--- src/cryo_sbi/utils/image_utils.py
+++ src/cryo_sbi/utils/image_utils.py
@@ -4,8 +4,53 @@ import mrcfile
 from cryo_sbi.wpa_simulator.noise import circular_mask
 
 
+class Mask:
+    def __init__(self, image_size, radius):
+        self.image_size = image_size
+        self.n_pixels = radius
+        self.mask = circular_mask(image_size, radius)
+
+    def __call__(self, image):
+        if len(image.shape) == 2:
+            image[cm] = 0
+        elif len(image.shape) == 3:
+            image[:, cm] = 0
+        else:
+            raise NotImplementedError
+
+        return image
+
+
+class FourierDownSample:
+    def __init__(self, image_size, n_pixels):
+        self.image_size = image_size
+        self.n_pixels = n_pixels
+
+    def __call__(self, image):
+        fft_image = torch.fft.fft2(image)
+        fft_image = torch.fft.fftshift(fft_image)
+
+        if len(image.shape) == 2:
+            fft_image = fft_image[
+                self.n_pixels : self.image_size - self.n_pixels,
+                self.n_pixels : self.image_size - self.n_pixels,
+            ]
+        elif len(image.shape) == 3:
+            fft_image = fft_image[
+                :,
+                self.n_pixels : self.image_size - self.n_pixels,
+                self.n_pixels : self.image_size - self.n_pixels,
+            ]
+        else:
+            raise NotImplementedError
+        fft_image = torch.fft.fftshift(fft_image)
+        reconstructed = torch.fft.ifft2(fft_image).real
+
+        return reconstructed
+
+
 class LowPassFilter:
-    def __init__(self, frequency_cutoff, image_size):
+    def __init__(self, image_size, frequency_cutoff):
         self.mask = circular_mask(image_size, frequency_cutoff)
 
     def __call__(self, image):
