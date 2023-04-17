2d2f838fc3e7f9e95ab34672049746b068fe6800
Dingel321 <dingel321@gmail.com>
2023-04-17 09:31:16 +0200

    added documentation in image utils

--- src/cryo_sbi/utils/image_utils.py
+++ src/cryo_sbi/utils/image_utils.py
@@ -5,6 +5,17 @@ import mrcfile
 
 
 def circular_mask(n_pixels, radius, inside=True):
+    """Create a circular mask for a given image size and radius.
+
+    Args:
+        n_pixels (int): Side length of the image in pixels.
+        radius (int): Radius of the circle.
+        inside (bool, optional): If True, the mask will be True inside the circle. Defaults to True.
+
+    Returns:
+        mask (torch.Tensor): Mask of shape (n_pixels, n_pixels).
+    """
+
     grid = torch.linspace(-0.5 * (n_pixels - 1), 0.5 * (n_pixels - 1), n_pixels)
     r_2d = grid[None, :] ** 2 + grid[:, None] ** 2
 
@@ -17,6 +28,17 @@ def circular_mask(n_pixels, radius, inside=True):
 
 
 class Mask:
+    """Mask a circular region in an image.
+
+    Args:
+        image_size (int): Number of pixels in the image.
+        radius (int): Radius of the circle.
+        inside (bool, optional): If True, the mask will be True inside the circle. Defaults to True.
+
+    Returns:
+        image (torch.Tensor): Masked image.
+    """
+
     def __init__(self, image_size, radius, inside=False):
         self.image_size = image_size
         self.n_pixels = radius
@@ -34,6 +56,17 @@ class Mask:
 
 
 def fourier_down_sample(image, image_size, n_pixels):
+    """Downsample an image by removing the outer frequencies.
+
+    Args:
+        image (torch.Tensor): Image of shape (n_pixels, n_pixels) or (n_channels, n_pixels, n_pixels).
+        image_size (int): Side length of the image in pixels.
+        n_pixels (int): Number of pixels to remove from each side.
+
+    Returns:
+        reconstructed (torch.Tensor): Downsampled image.
+    """
+
     fft_image = torch.fft.fft2(image)
     fft_image = torch.fft.fftshift(fft_image)
 
@@ -57,6 +90,16 @@ def fourier_down_sample(image, image_size, n_pixels):
 
 
 class FourierDownSample:
+    """Downsample an image by removing the outer frequencies.
+
+    Args:
+        image_size (int): Side length of the image in pixels.
+        down_sampled_size (int): Number of pixels in the downsampled image.
+
+    Returns:
+        down_sampled (torch.Tensor): Downsampled image.
+    """
+
     def __init__(self, image_size, down_sampled_size):
         self._image_size = image_size
         self._n_pixels = (image_size - down_sampled_size) // 2
@@ -70,6 +113,16 @@ class FourierDownSample:
 
 
 class LowPassFilter:
+    """Low pass filter an image by removing the outer frequencies.
+
+    Args:
+        image_size (int): Side length of the image in pixels.
+        frequency_cutoff (int): Frequency cutoff.
+
+    Returns:
+        reconstructed (torch.Tensor): Low pass filtered image.
+    """
+
     def __init__(self, image_size, frequency_cutoff):
         self.mask = circular_mask(image_size, frequency_cutoff)
 
@@ -88,6 +141,15 @@ class LowPassFilter:
 
 
 class NormalizeIndividual:
+    """Normalize an image by subtracting the mean and dividing by the standard deviation.
+
+    Args:
+        images (torch.Tensor): Image of shape (n_channels, n_pixels, n_pixels).
+
+    Returns:
+        normalized (torch.Tensor): Normalized image.
+    """
+
     def __init__(self) -> None:
         pass
 
@@ -98,6 +160,15 @@ class NormalizeIndividual:
 
 
 class MRCtoTensor:
+    """Convert an MRC file to a tensor.
+
+    Args:
+        image_path (str): Path to the MRC file.
+
+    Returns:
+        image (torch.Tensor): Image of shape (n_pixels, n_pixels).
+    """
+
     def __init__(self) -> None:
         pass
 
@@ -109,7 +180,17 @@ class MRCtoTensor:
 
 
 class AddLowFrequencyNoise:
-    """Adding low frequency noise, also serving as a low pass filter"""
+    """Adding low frequency noise, also serving as a low pass filter.
+
+    Args:
+        image_size (int): Side length of the image in pixels.
+        min_frequency (int): Minimum frequency to keep.
+        amplitude (float or list): Amplitude of the noise. If a float, the noise will have the same amplitude everywhere. If a list, the noise will have a uniform distribution between the two values.
+        device (str, optional): Device to use. Defaults to "cpu".
+
+    Returns:
+        image (torch.Tensor): Image with added noise.
+    """
 
     def __init__(self, image_size, min_frequency, amplitude, device="cpu") -> None:
         self.device = device
