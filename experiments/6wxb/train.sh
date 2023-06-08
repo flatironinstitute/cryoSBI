c2563ca203e5de490e41a87d5be90b59f790643c
Dingel321 <Dingel321@gmail.com>
2023-06-07 21:47:08 -0400

    changed training parameters

--- experiments/6wxb/train.sh
+++ experiments/6wxb/train.sh
@@ -2,9 +2,8 @@ n_workers=$(nproc)
 train_npe_model \
     --image_config_file 6wxb/image_params_mixed_training.json \
     --train_config_file 6wxb/resnet18_fft_encoder.json\
-    --epochs 300 \
+    --epochs 600 \
     --estimator_file 6wxb/posterior_6wxb_mixed.estimator \
     --loss_file 6wxb/posterior_6wxb_mixed.loss \
     --n_workers $n_workers \
-    --train_device cuda \
-    --whitening_filter /mnt/home/ldingeldein/ceph/cryo_sbi_data/whitening_filter/average_psd_downsampled_128.pt
+    --train_device cuda 
