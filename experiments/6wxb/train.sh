1688086e2c57793b2bbbfeb53b2207b4a73cbb22
Dingel321 <dingel321@gmail.com>
2023-10-20 12:00:50 +0200

    removed old notebooks and experiment files

--- experiments/6wxb/train.sh
+++
@@ -1,9 +0,0 @@
-n_workers=$(nproc)
-train_npe_model \
-    --image_config_file 6wxb/image_params_bending_training.json \
-    --train_config_file 6wxb/resnet18_fft_encoder.json\
-    --epochs 600 \
-    --estimator_file 6wxb/posterior_6wxb_bending.estimator \
-    --loss_file 6wxb/posterior_6wxb_bending.loss \
-    --n_workers $n_workers \
-    --train_device cuda 
--- experiments/benchmark_hsp90/train.sh
+++
@@ -1,7 +0,0 @@
-python3 -m cryo_sbi.inference.NPE_train_without_saving \
-    --image_config_file benchmark_hsp90/image_params_training.json \
-    --train_config_file benchmark_hsp90/resnet18_encoder.json\
-    --epochs 200 \
-    --estimator_file benchmark_hsp90/posterior_hsp90.estimator \
-    --loss_file benchmark_hsp90/posterior_hsp90.loss \
-    --n_workers 24
